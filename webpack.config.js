446067ac3805f2b0bf13bc7664a51fc95d4cdc2a
Nguyen <anhln@mor.com.vn>
2023-06-16 16:39:35 +0700

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,23 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js"
+    filename: "index_bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        use: ["babel-loader"],
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+        test: /\.(js|jsx)$/,
+        options: {
+          presets: ["@babel/env", "@babel/preset-react"],
+          babelrcRoots: ["../*"],
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
