c9f98696582fd03cacddee84f9a7f1b520d1a346
Nguyen <anhln@mor.com.vn>
2023-06-16 16:25:44 +0700

    config webpack

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,6 @@
 module.exports = {
-  // presets: [
-  //   ['@babel/preset-env', {targets: {node: 'current'}}],
-  //   ['@babel/preset-react', {targets: {node: 'current'}}] // add this
-  // ]
-
-    "plugins": [ "@babel/plugin-proposal-logical-assignment-operators" ],
-    "presets": [
-      [
-        "babel-preset-gatsby",
-        {
-          "targets": {
-            "browsers": [
-              ">0.25%",
-              "not dead"
-            ]
-          }
-        }
-      ]
-    ]
-
+  presets:[
+      "@babel/preset-env",
+      "@babel/preset-react"
+  ]
 }
